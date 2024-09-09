4b9d529617b0ace19a7f4caf46b63e4f134c6054
HristoSL <hristoivanovslavchev@gmail.com>
2024-09-09 20:13:17 +0300

    Initial commit

---
+++ src/main/java/com/sirmaacademy/sirmafinalexam/SirmaFinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.sirmaacademy.sirmafinalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirmaFinalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirmaFinalExamApplication.class, args);
+	}
+
+}
