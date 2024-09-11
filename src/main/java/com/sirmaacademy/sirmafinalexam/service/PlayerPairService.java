f935821a9c58eaf292d03543bd6019f55382d2b3
HristoSL <hristoivanovslavchev@gmail.com>
2024-09-11 23:30:01 +0300

    minor spelling fixes.

--- src/main/java/com/sirmaacademy/sirmafinalexam/service/PlayerPairService.java
+++ src/main/java/com/sirmaacademy/sirmafinalexam/service/PlayerPairService.java
@@ -35,8 +35,6 @@ public class PlayerPairService {
                 biggestOverlapInMinutes = playerPair.getTotalOverlappedMinutes();
             }
         }
-
-
         return mostOverlappingPair;
     }
 
